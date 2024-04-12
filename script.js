5cbce5e8ec3557e21ee99c30fb7fee649b921507
YASH_GAWADE <133790696+l3nox-music@users.noreply.github.com>
2024-04-12 16:31:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuIcon = document.querySelector('.hamburger');
 const menuContent = document.querySelector('.menu');
 function toggleMenu(){
-  menuIcon.classList.toggle("visible")
+  menuContent.classList.toggle("visible")
 }
